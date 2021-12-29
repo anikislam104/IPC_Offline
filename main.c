fc41a7b3284666dffec7ee43a9ac821070758f51
Anik Islam Pantha <1705104@ugrad.cse.buet.ac.bd>
2021-12-30 01:29:14 +0600

    working on offline kiosk part

--- main.c
+++ main.c
@@ -3,9 +3,12 @@
 #include <stdlib.h>
 #include "unistd.h"
 #include "semaphore.h"
-#define NUM_THREADS	2
+#define NUM_THREADS	7
+int M;
 sem_t kioskEmpty;
 sem_t kioskFull;
+int KIOSK[10];
+int kioskIndex;
 struct passenger_status{
     int id;
     int isVIP;
@@ -13,25 +16,37 @@ struct passenger_status{
 
 struct passenger_status passengerStatusArray[NUM_THREADS];
 
-void *Process(void *threadid) {
+void *Process(void *threadarg) {
+    int passID,vip;
+    struct passenger_status *passengerStatus;
+    passengerStatus=(struct passenger_status *)threadarg;
+    passID=passengerStatus->id;
+    vip=passengerStatus->isVIP;
+    printf("id=%d \nvip=%d \n",passID,vip);
+    int index=rand()%10;
+    //printf("%d\n",index);
 
+    pthread_exit(NULL);
 
 }
 int main (int argc, char *argv[]) {
     pthread_t passengers[NUM_THREADS];
     int rc;
-    int M;
-    printf("Input M:");
-    scanf("%d",M);
-    printf("\n");
-    int KIOSK[M];
+//    printf("Input M:");
+//    scanf("%d\n",M);
+//    printf("\n");
+//    printf("%d\n",M);
+//    for(int i=0;i<M;i++){
+//        KIOSK[i]=-1;
+//    }
+//    printf("%d",KIOSK[M-2]);
     sem_init(&kioskEmpty,0,M);
     for(int t=0; t<NUM_THREADS; t++){
         passengerStatusArray[t].id=t;
         if(t%2==0){
-            passengerStatusArray->isVIP=1;
+            passengerStatusArray[t].isVIP=1;
         } else{
-            passengerStatusArray->isVIP=0;
+            passengerStatusArray[t].isVIP=0;
         }
         printf("Passenger generated:%d\n",t);
         rc= pthread_create(&passengers[t],NULL,Process,(void *)&passengerStatusArray[t]);
