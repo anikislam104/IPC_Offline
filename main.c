0ff7dba504d004c5ea5dd3256c4207abf7978c85
Anik Islam Pantha <1705104@ugrad.cse.buet.ac.bd>
2021-12-30 04:21:38 +0600

    kiosk belt boarding vip channel airport

--- main.c
+++ main.c
@@ -4,14 +4,27 @@
 #include "unistd.h"
 #include "semaphore.h"
 #define NUM_THREADS	7
-#define M 7
-#define N 5
+#define M 2
+#define N 3
 #define P 5
 sem_t kioskEmpty;
 sem_t kioskFull;
 pthread_mutex_t mutex;
 int KIOSK[10];
 int kioskIndex;
+
+struct Boarding{
+    int passenger[M*N*P];
+    int currentPassenger;
+};
+struct VIPchannel{
+    int passengerGoing[M*N*P];
+    int passengerWaiting[M*N*P];
+    int PassengerComingBack[M*N*P];
+    int going;
+    int waiting;
+    int comingBack;
+};
 struct passenger{
     int id;
     int isVIP;
@@ -25,11 +38,18 @@ struct Belt{
 struct Kiosk{
     struct Belt belts[N];
 };
+
+struct Airport{
+    struct Kiosk kiosks[M];
+    struct Boarding boarding;
+    struct VIPchannel viPchannel;
+};
+struct Airport airport;
 struct passenger passengerArray[NUM_THREADS];
-struct Kiosk kiosks[M];
+//struct Kiosk kiosks[M];
 
 int isKioskFull(int kioskIndex,int beltIndex){
-    if(kiosks[kioskIndex].belts[beltIndex].currentPassenger==P){
+    if(airport.kiosks[kioskIndex].belts[beltIndex].currentPassenger==P){
         return 1;
     } else{
         return 0;
@@ -42,21 +62,25 @@ void *Process(void *threadarg) {
     passengerStatus=(struct passenger_status *)threadarg;
     passID=passengerStatus->id;
     vip=passengerStatus->isVIP;
-    printf("id=%d \nvip=%d \n",passID,vip);
+    //printf("id=%d \nvip=%d \n",passID,vip);
     pthread_mutex_lock(&mutex);
     int hasEntered=0;
-    while (hasEntered==0){
-        int kioskIndex=rand()%M;
-        int beltIndex=rand()%N;
-        //printf("kiosk idx %d belt idx %d\n",kioskIndex,beltIndex);
-        int isFull= isKioskFull(kioskIndex,beltIndex);
-        if(isFull==1){
-            printf("Passenger %d Kiosk %d Belt %d is full\n",passID,kioskIndex,beltIndex);
-        } else{
-            printf("Passenger %d is going through Kiosk %d Belt %d\n",passID,kioskIndex,beltIndex);
-            kiosks[kioskIndex].belts[beltIndex].currentPassenger++;
-            hasEntered=1;
+    if(vip==0) {
+        while (hasEntered == 0) {
+            int kioskIndex = rand() % M;
+            int beltIndex = rand() % N;
+            //printf("kiosk idx %d belt idx %d\n",kioskIndex,beltIndex);
+            int isFull = isKioskFull(kioskIndex, beltIndex);
+            if (isFull == 1) {
+                printf("Passenger %d Kiosk %d Belt %d is full\n", passID, kioskIndex, beltIndex);
+            } else {
+                printf("Passenger %d is going through Kiosk %d Belt %d\n", passID, kioskIndex, beltIndex);
+                airport.kiosks[kioskIndex].belts[beltIndex].passengers[airport.kiosks[kioskIndex].belts[beltIndex].currentPassenger] = passID;
+                airport.kiosks[kioskIndex].belts[beltIndex].currentPassenger++;
+                hasEntered = 1;
+            }
         }
+
     }
     pthread_mutex_unlock(&mutex);
     //printf("%d\n",index);
@@ -78,10 +102,10 @@ int main (int argc, char *argv[]) {
 //    printf("%d",KIOSK[M-2]);
     for(int i=0;i<M;i++){
         for(int j=0;j<N;j++){
-            kiosks[i].belts[j].currentPassenger=0;
-            kiosks[i].belts[j].maximumPassenger=P;
+            airport.kiosks[i].belts[j].currentPassenger=0;
+            airport.kiosks[i].belts[j].maximumPassenger=P;
             for(int k=0;k<5;k++){
-                kiosks[i].belts[j].passengers[k]=-1;
+                airport.kiosks[i].belts[j].passengers[k]=-1;
                 //printf("Kiosk %d belt %d passenger number %d\n",i,j,k);
             }
         }
@@ -94,7 +118,7 @@ int main (int argc, char *argv[]) {
         } else{
             passengerArray[t].isVIP=0;
         }
-        printf("Passenger generated:%d\n",t);
+        //printf("Passenger generated:%d\n",t);
         rc= pthread_create(&passengers[t],NULL,Process,(void *)&passengerArray[t]);
         if(rc){
             printf("ERROR; return code from pthread_create() is %d\n", rc);
