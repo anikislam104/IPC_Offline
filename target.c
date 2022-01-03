b372236c6682dc46874d2d06c816971c41482a68
Anik Islam Pantha <1705104@ugrad.cse.buet.ac.bd>
2022-01-03 20:49:21 +0600

    poisson ongoing removed comment outs

--- target.c
+++ target.c
@@ -5,6 +5,8 @@
 
 #include <stdio.h>
 #include "math.h"
+#include "time.h"
+#include "unistd.h"
 int factorial(int n)
 {
     int s=1;
@@ -15,24 +17,45 @@ int factorial(int n)
 }
 double poissonDist(int lambda,int x)
 {
-    printf("%f  ",(double )lambda);
+    //printf("%f  ",(double )lambda);
     double a= exp(-1.0*(double )lambda);
-    printf("%f   ",a);
+    //printf("%f   ",a);
     a=a*pow((double )lambda,(double )x);
     a=a/(double ) factorial(x);
     int ans=(int )a;
     return a;
 }
+double TestFunction(int max, int numIntervals,int x) {
+
+    //int intervals[numIntervals];
+
+    double exponent = 2;
+
+    double n = pow(exponent, numIntervals) - 1;
+    double d = exponent - 1;
+
+    double t = max / (n / d);
+
+
+    double interval = t * pow(exponent, x);
+    return interval;
+
+}
 int main(){
-    int lambda,x;
-    lambda=1;
-    x=1;
-    printf("%f\n", poissonDist(lambda,x));
-    lambda=1;
-    x=2;
-    printf("%f\n", poissonDist(lambda,x));
-    lambda=1;
-    x=3;
-    printf("%f\n", poissonDist(lambda,x));
+    int lambda,x=1;
+    lambda=10;
+    int maxTime=150000;
+    int timePassed=0;
+    for(int i=0;i<100;i++){
+        if(x>lambda) {
+            x = 1;
+            timePassed+=maxTime-timePassed;
+        }
+        int s=(int)(round(poissonDist(lambda,x)*1000000));
+        timePassed+=s/10;
+        printf("%d %d\n",timePassed,s);
+        sleep((int)s/100000);
+        x++;
+    }
 
 }
